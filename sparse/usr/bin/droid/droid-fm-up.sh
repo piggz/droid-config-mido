31a111636a083810982248cb574949c3eb276297
Adam Pigg <adam@piggz.co.uk>
2017-08-11 08:34:06 +0100

    Radio module no longer has this param

--- sparse/usr/bin/droid/droid-fm-up.sh
+++ sparse/usr/bin/droid/droid-fm-up.sh
@@ -2,7 +2,6 @@
 
 setprop hw.fm.init 0
 /sbin/modprobe radio-iris-transport
-echo 1 > /sys/module/radio_iris_transport/parameters/fmsmd_set
 /system/bin/fm_qsoc_patches 199217 0
 setprop hw.fm.init 1
  
