0fab0ac1452e7e50ee67512f0cc8eae7c0657232
Adam Pigg <adam@piggz.co.uk>
2017-10-03 19:14:27 +0100

    Dont exec droid-hal-init

---
+++ sparse/usr/bin/droid/droid-hal-startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /
+touch /dev/.coldboot_done
+export LD_LIBRARY_PATH=/usr/libexec/droid-hybris/system/lib/:/vendor/lib:/system/lib
+
+# Save systemd notify socket name to let droid-init-done.sh pick it up later
+echo $NOTIFY_SOCKET > /run/droid-hal/notify-socket-name
+
+/sbin/droid-hal-init
+
