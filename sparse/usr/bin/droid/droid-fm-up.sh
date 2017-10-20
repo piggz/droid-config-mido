aee95c6f94f77078df445c15e9444f1edffdaa9e
Adam Pigg <adam@piggz.co.uk>
2017-10-20 18:00:31 +0100

    Allow radio to be init/de-init

--- sparse/usr/bin/droid/droid-fm-up.sh
+++ sparse/usr/bin/droid/droid-fm-up.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-setprop hw.fm.init 0
-/sbin/modprobe radio-iris-transport
-/system/bin/fm_qsoc_patches 199217 0
-setprop hw.fm.init 1
- 
+chmod a+w /sys/module/radio_iris_transport/parameters/fmsmd_set
+
